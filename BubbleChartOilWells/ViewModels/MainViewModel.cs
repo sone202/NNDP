b2a6a920b0eabe14cf19fa3840ca4480e0f2386c
Timur Zinnatullin <timzlinn@gmail.com>
2020-10-17 22:28:02 +0500

    refactoring MainViewModel

--- BubbleChartOilWells/ViewModels/MainViewModel.cs
+++ BubbleChartOilWells/ViewModels/MainViewModel.cs
@@ -22,160 +22,143 @@ namespace BubbleChartOilWells.ViewModels
 {
     public class MainViewModel : BaseViewModel
     {
-        static private List<Bubble> _oil_wells;
-        static public Bubble _current_selected;
 
-        static private Dictionary<Bubble, OilWell> _data_Bubble_OilWell = new Dictionary<Bubble, OilWell>();
-        //static private Dictionary<Path, OilWell> _data_Path_OilWell = new Dictionary<Path, OilWell>();
-        //static private Dictionary<Path, Bubble> _data_Path_Bubble = new Dictionary<Path, Bubble>();
+        private bool fileImportIsBusy = false;
+        private bool treeVisibility = false;
+        private bool settingsVisibility = false;
+        private bool anySelected = false;
+        private string wellInfo;
+
+        private AsyncCommand importFileAsyncCommand;
+        private AsyncCommand openSettingsAsyncCommand;
+        private AsyncCommand openTreeAsyncCommand;
+        private AsyncCommand bubbleSelectingAsyncCommand;
+
+        static private List<Bubble> oilWell;
+        static public Bubble CurrentSelected;
+
+        static private Dictionary<Bubble, OilWell> dataBubbleOilWell = new Dictionary<Bubble, OilWell>();
+
+        static public ObservableCollection<object> OilWellsPaths { get; private set; } = new ObservableCollection<object>();
 
 
-        static public ObservableCollection<object> oil_wells_paths { get; private set; } = new ObservableCollection<object>();
-
-
-        private bool _fileImport_isBusy = false;
-        public bool fileImport_isBusy
+        public bool FileImportIsBusy
         {
-            get => _fileImport_isBusy;
+            get => fileImportIsBusy;
             set
             {
-                if (_fileImport_isBusy != value)
+                if (fileImportIsBusy != value)
                 {
-                    _fileImport_isBusy = value;
+                    fileImportIsBusy = value;
                     ImportFileAsyncCommand.RaiseCanExecuteChanged();
                 }
             }
         }
-
-        private bool _tree_visibility = false;
-        public bool tree_visibility
+        public bool TreeVisibility
         {
-            get => _tree_visibility;
+            get => treeVisibility;
             set
             {
-                _tree_visibility = value;
-                OnPropertyChanged(nameof(tree_visibility));
+                treeVisibility = value;
+                OnPropertyChanged(nameof(TreeVisibility));
+            }
+        }
+        public bool SettingsVisibility
+        {
+            get => settingsVisibility;
+            set
+            {
+                settingsVisibility = value;
+                OnPropertyChanged(nameof(SettingsVisibility));
+            }
+        }
+        public bool AnySelected
+        {
+            get => anySelected;
+            set
+            {
+                anySelected = value;
+                OnPropertyChanged(nameof(AnySelected));
+            }
+        }
+        public string WellInfo
+        {
+            get => wellInfo;
+            set
+            {
+                wellInfo = value;
+                OnPropertyChanged(nameof(WellInfo));
             }
         }
 
 
-        private bool _settings_visibility = false;
-        public bool settings_visibility
-        {
-            get => _settings_visibility;
-            set
-            {
-                _settings_visibility = value;
-                OnPropertyChanged(nameof(settings_visibility));
-            }
-        }
-
-
-        private bool _any_selected = false;
-        public bool any_selected
-        {
-            get => _any_selected;
-            set
-            {
-                _any_selected = value;
-                OnPropertyChanged(nameof(any_selected));
-            }
-        }
-
-
-        private string _well_info;
-        public string well_info
-        {
-            get => _well_info;
-            set
-            {
-                _well_info = value;
-                OnPropertyChanged(nameof(well_info));
-            }
-        }
-
-
-
-
-
-
-
-        private AsyncCommand _importFileAsyncCommand;
         public AsyncCommand ImportFileAsyncCommand
         {
-            get { return _importFileAsyncCommand ?? (_importFileAsyncCommand = new AsyncCommand(FileImportAsync, CanExecute)); }
+            get { return importFileAsyncCommand ?? (importFileAsyncCommand = new AsyncCommand(FileImportAsync, CanExecute)); }
         }
         private async Task FileImportAsync()
         {
             // adding wells to the grid
             try
             {
-                fileImport_isBusy = true;
-                await Task.Run(() => _oil_wells = DataImport.GetDataList());
+                FileImportIsBusy = true;
+                await Task.Run(() => oilWell = DataImport.GetDataList());
             }
             catch (Exception e)
             {
                 MessageBox.Show(e.Message);
-                fileImport_isBusy = false;
+                FileImportIsBusy = false;
             }
-            if (_oil_wells?.Count > 0)
-                foreach (var bubble in _oil_wells)
+            if (oilWell?.Count > 0)
+                foreach (var bubble in oilWell)
                 {
                     bubble.Update();
                     foreach (var path in bubble.paths)
                     {
-                        oil_wells_paths.Add(path);
+                        OilWellsPaths.Add(path);
                     }
-                    oil_wells_paths.Add(bubble.ID);
+                    OilWellsPaths.Add(bubble.ID);
 
-                    _data_Bubble_OilWell[bubble] = bubble.data;
+                    dataBubbleOilWell[bubble] = bubble.data;
                 }
 
-            fileImport_isBusy = false;
+            FileImportIsBusy = false;
         }
         private bool CanExecute(object sender)
         {
-            return !fileImport_isBusy;
+            return !FileImportIsBusy;
         }
 
-
-        private AsyncCommand _openSettingsAsyncCommand;
         public AsyncCommand OpenSettingsAsyncCommand
         {
-            get { return _openSettingsAsyncCommand ?? (_openSettingsAsyncCommand = new AsyncCommand(OpenSettingsAsync)); }
+            get { return openSettingsAsyncCommand ?? (openSettingsAsyncCommand = new AsyncCommand(OpenSettingsAsync)); }
         }
         private async Task OpenSettingsAsync()
         {
-            await Task.Run(() => settings_visibility = settings_visibility == false ? true : false);
+            await Task.Run(() => SettingsVisibility = SettingsVisibility == false ? true : false);
         }
 
-
-
-        private AsyncCommand _openTreeAsyncCommand;
         public AsyncCommand OpenTreeAsyncCommand
         {
-            get { return _openTreeAsyncCommand ?? (_openTreeAsyncCommand = new AsyncCommand(OpenTreeAsync)); }
+            get { return openTreeAsyncCommand ?? (openTreeAsyncCommand = new AsyncCommand(OpenTreeAsync)); }
         }
         private async Task OpenTreeAsync()
         {
-            await Task.Run(() => tree_visibility = tree_visibility == false ? true : false);
+            await Task.Run(() => TreeVisibility = TreeVisibility == false ? true : false);
         }
 
-
-        private AsyncCommand _bubbleSelectingAsyncCommand;
         public AsyncCommand BubbleSelectingAsyncCommand
         {
-            get { return _bubbleSelectingAsyncCommand ?? (_bubbleSelectingAsyncCommand = new AsyncCommand(BubbleSelectingAsync)); }
+            get { return bubbleSelectingAsyncCommand ?? (bubbleSelectingAsyncCommand = new AsyncCommand(BubbleSelectingAsync)); }
         }
-
         private async Task BubbleSelectingAsync()
         {
             // getting element under mouse cursor
             var el = Mouse.DirectlyOver;
             Bubble clicked_well = null;
-            if (oil_wells_paths.Contains(el))
+            if (OilWellsPaths.Contains(el))
             {
-                System.Threading.Tasks.Parallel.ForEach(_oil_wells, well =>
+                System.Threading.Tasks.Parallel.ForEach(oilWell, well =>
                 {
                     if (well.Contains(el))
                     {
@@ -183,27 +166,26 @@ namespace BubbleChartOilWells.ViewModels
                         return;
                     }
                 });
-                _current_selected?.Unselect();
+                CurrentSelected?.Unselect();
                 clicked_well?.Select();
-                _current_selected = clicked_well;
-                any_selected = true;
+                CurrentSelected = clicked_well;
+                AnySelected = true;
             }
             else
             {
-                _current_selected?.Unselect();
-                _current_selected = null;
-                any_selected = false;
+                CurrentSelected?.Unselect();
+                CurrentSelected = null;
+                AnySelected = false;
             }
-            if (_current_selected != null)
-                well_info = "Номер скважины: " + _current_selected.data.ID +
-                "\nX = " + _current_selected.data.X +
-                "\nY = " + _current_selected.data.Y +
-                "\nТекущий дебит нефти = " + _current_selected.data.oil_debit + " т/сут" +
-                "\nТекущий дебит жидкости = " + _current_selected.data.liquid_debit + " т/сут" +
-                "\nНакопленная добыча нефти = " + _current_selected.data.oil_prod + " тыс. т" +
-                "\nНакопленная добыча жидкости = " + _current_selected.data.liquid_prod + " тыс. т";
+            if (CurrentSelected != null)
+                WellInfo = "Номер скважины: " + CurrentSelected.data.ID +
+                "\nX = " + CurrentSelected.data.X +
+                "\nY = " + CurrentSelected.data.Y +
+                "\nТекущий дебит нефти = " + CurrentSelected.data.oil_debit + " т/сут" +
+                "\nТекущий дебит жидкости = " + CurrentSelected.data.liquid_debit + " т/сут" +
+                "\nНакопленная добыча нефти = " + CurrentSelected.data.oil_prod + " тыс. т" +
+                "\nНакопленная добыча жидкости = " + CurrentSelected.data.liquid_prod + " тыс. т";
         }
-
     }
 }
 
